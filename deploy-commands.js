ecb55c7851b1d344ba24b4ea5c227fa83e89ea5a
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-04-23 01:30:12 +0800

    Add new commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,35 +1,53 @@
-// const { SlashCommandBuilder } = require('@discordjs/builders');
-// const { REST } = require('@discordjs/rest');
-// const { Routes } = require('discord-api-types/v9');
-// const { clientId, guildId, token } = require('./config.json');
-// const fs = require('node:fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require('dotenv').config();
 
-// const commands = [
-// 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-// 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-// 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-// ];
+const TOKEN = process.env.DISCORD_TOKEN;
+const CLIENTID = process.env.CLIENT_ID;
+const GUILDID = process.env.GUILD_ID;
+const MIDTOWNID = process.env.MIDTOWN_ID;
+const KSLID = process.env.KSL_ID;
 
-// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const fs = require('node:fs');
 
-// for (const file of commandFiles) {
-// 	const command = require(`./commands/${file}`);
-// 	commands.push(command.data.toJSON());
-// }
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new SlashCommandBuilder().setName('yourmom').setDescription('Something about your mom'),
+];
 
-// const rest = new REST({ version: '9' }).setToken(token);
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// (async () => {
-// 	try {
-// 		console.log('Started refreshing application (/) commands.');
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
-// 		await rest.put(
-// 			Routes.applicationGuildCommands(clientId, guildId),
-// 			{ body: commands },
-// 		);
+const rest = new REST({ version: '9' }).setToken(TOKEN);
 
-// 		console.log('Successfully reloaded application (/) commands.');
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// })();
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(CLIENTID, GUILDID),
+			{ body: commands },
+		);
+
+        await rest.put(
+			Routes.applicationGuildCommands(CLIENTID, MIDTOWNID),
+			{ body: commands },
+		);
+
+        await rest.put(
+			Routes.applicationGuildCommands(CLIENTID, KSLID),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
--- index.js
+++ index.js
@@ -15,4 +15,21 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+		await interaction.reply(`Server info: \nName: ${interaction.guild.name} \nTotal Members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	} else if (commandName === 'yourmom') {
+        await interaction.reply('Your mom');
+        await interaction.followUp('is gay');
+    }
+});
+
 client.login(TOKEN);
